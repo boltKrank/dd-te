cee596992ef0fa488dd8e978bb0d980cb12dc4ef
Simon Anderson <simon.anderson111@gmail.com>
2022-06-17 22:40:34 +1000

    Both functions working

--- day-one/Revhash.java
+++ day-one/Revhash.java
@@ -11,47 +11,28 @@ class Revhash {
 
         String randWord = randomWords[wordPos];
         
-
         StringBuilder hashreverser = new StringBuilder();
         hashreverser.append(orignalString);
         hashreverser.reverse();
         hashreverser.replace(randWord.length(), randWord.length()+1, randWord.substring(0, 1));
 
         String password = randWord + orignalString.charAt((orignalString.length()-randWord.length())-1);
-
-        // Change originalString charPlace wordPos with first letter of newHashWord + replaced letter at end of password
-        return "New hash is: " + hashreverser + " Password is: "+ password + " randword length is: " + randWord.length();
-
+        
+        return "Encrypted hash is: " + hashreverser + " Password is: " + password;
     }
 
     public String revHashDecrypt(String encryptedHash, String password) {
 
-        String testHash = "844dc96pcf878f5e4e576221772329b6";
-        String testPassword = "pizzazz7";
-
         StringBuilder hashDecrypter = new StringBuilder();
-        hashDecrypter.append(testHash);
-        //hashreverser.replace(randWord.length(), randWord.length()+1, randWord.substring(0, 1));
+        hashDecrypter.append(encryptedHash);
+        hashDecrypter.replace(password.length()-1, password.length(), password.substring(password.length()-1, password.length()));
         hashDecrypter.reverse();
 
-        // replace testHash.chat at testPassword.length -1 with charAt(0)
-        // reverse testHash
-        // return newHash
-
         return "Decrypted hash is: " + hashDecrypter;
-
     }
     
 
     public static void main(String[] args) {
-
-        // add rev and unrev
-
-        // get hash, change character at position
-        // password length shows position, first letter of password is correct character
-
-        //Check usage:
-        System.out.println(args.length);
 
         if(args.length == 2 && args[0].equals("encrypt")){
             if(args[1].length() != 32){
@@ -73,11 +54,8 @@ class Revhash {
             System.out.println(rh.revHashDecrypt(args[1],args[2]));        
         }
         else {
-            // Print usage
             System.out.println("Usage:  Revhash < encrpt | decrypt> hash [password]");
             System.out.println("Password is needed for decrypt");
         }
-
     }
-
 }
